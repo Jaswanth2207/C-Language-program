827b0ee2946a98a652f3a5e979845f33e6df5123
Jaswanth <155299522+Jaswanth2207@users.noreply.github.com>
2024-02-13 20:22:27 +0530

    Create Number is positive or negative.c

---
+++ Number is positive or negative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter A number: ");
+    scanf("%d",&a);
+    printf("enter B number: ");
+    scanf("%d",&b);
+    if (a>0 && b>0)
+    {
+        printf("The given two numbers are positive");
+    }
+    return 0;
+}
