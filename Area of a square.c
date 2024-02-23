d860ae04e68712fe36b64a6ceb735d752be6ab92
Jaswanth <155299522+Jaswanth2207@users.noreply.github.com>
2024-02-23 20:45:09 +0530

    Create Area of a square.c
    in area you can formulas for various areas like rectangle , circle, rhombus etc.

---
+++ Area of a square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter side of the square: ");
+    scanf("%d",&a);
+    int area=a*a;
+    printf("The area of the square is : %d",area);
+    return 0;
+}
