58520a80c47c7d0998e8f79d3e4ea4ce37240dda
Jaswanth <155299522+Jaswanth2207@users.noreply.github.com>
2024-03-04 06:55:03 +0530

    Create Addition.c
    This program is to add 2 numbers taking by the user.

---
+++ Addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter 1st value: ");
+    scanf("%d",&a);
+    printf("enter 2nd value: ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum of the given 2 numbers is : %d",c);
+    return 0;
+}
