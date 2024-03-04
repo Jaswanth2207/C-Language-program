13fbf986f921d5f1e434f97c9b1327473961f246
Jaswanth <155299522+Jaswanth2207@users.noreply.github.com>
2024-03-04 20:51:51 +0530

    Create 1DArray.c
    here is the program for 1 dimensional array

---
+++ 1DArray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int j,a[5];
+    for (j=0;j<5;j++)
+    {
+        scanf("%d",&a[j]);
+    }
+    for (j=0;j<5;j++)
+    {
+        printf("%d",a[j]);
+    }
+    return 0;
+}
